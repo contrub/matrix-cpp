19387b1711134cfaf920b29002769cd83a4a260b
contrub <572519065z@gmail.com>
2022-03-11 00:06:10 +0200

    added matrix operators examples

--- main.cpp
+++ main.cpp
@@ -5,17 +5,23 @@
 using namespace std;
 
 int main() {
+    const int n = 7;
     Matrix m1(5, 5);
     Matrix m2(5, 5);
 
     m1.populateRandom();
     m2.populateRandom();
 
-    cout << m1;
-    cout << m2;
+    cout << m1 << endl;
+    cout << m2 << endl;
 
-    cout << m1.trace() << endl;
-    cout << m1.transpose();
+    cout << m1.transpose() << endl;
+
+    cout << m1 + m2 << endl;
+    cout << m2 - m1 << endl;
+    cout << n * m1 << endl;
+
+    cout << m1(3, 4) << endl;
 
     return 0;
 }
